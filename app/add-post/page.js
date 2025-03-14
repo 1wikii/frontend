2082ff8c352dd25b4d25ed6e5a5cf3cf1696ffbc
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-03-14 17:16:09 +0700

    update

--- app/add-post/page.js
+++ app/add-post/page.js
@@ -4,13 +4,13 @@ import { addPost } from "@/lib/api";
 import { useRouter } from "next/navigation";
 
 export default function AddPost() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [brand, setTitle] = useState("");
+  const [name, setContent] = useState("");
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await addPost({ title, content, published: false });
+    await addPost({ brand, name, published: false });
     router.push("/");
   };
 
@@ -19,9 +19,9 @@ export default function AddPost() {
       <h1 className="text-2xl font-bold mb-4">Tambah Post</h1>
       <form onSubmit={handleSubmit} className="space-y-4">
         <input type="text" placeholder="Title" className="border p-2 w-full"
-          value={title} onChange={(e) => setTitle(e.target.value)} />
+          value={brand} onChange={(e) => setTitle(e.target.value)} />
         <textarea placeholder="Content" className="border p-2 w-full"
-          value={content} onChange={(e) => setContent(e.target.value)} />
+          value={name} onChange={(e) => setContent(e.target.value)} />
         <button type="submit" className="bg-blue-500 text-white px-4 py-2">Tambah</button>
       </form>
     </div>
