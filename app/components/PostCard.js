4e73d9b6042e24dac2677b6b549c97ee264dbc4e
1wikii <ahmaddwikyzerrodixxon@gmail.com>
2025-03-14 17:12:02 +0700

    update

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -2,8 +2,8 @@
 export default function PostCard({ post, onDelete }) {
   return (
     <div className="border p-3 mb-2">
-      <h2 className="font-bold text-black">{post.title}</h2>
-      <p>{post.content}</p>
+      <h2 className="font-bold text-black">{post.brand}</h2>
+      <p>{post.name}</p>
       <button onClick={() => onDelete(post.id)} className="bg-red-500 text-white px-2 py-1 mt-2">Hapus</button>
     </div>
   );
